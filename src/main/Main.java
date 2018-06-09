6f7d677ee1b3c16b6643db306f6e37d8fed73734
daniel <danielk.kock@gmail.com>
2018-06-09 16:40:52 -0300

    algoritmo baseado em permissao finalizado

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,10 +5,9 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		// instancia o algoritmo de exclusão mútua baseada em anel
-		// com 4 nodos
+		//RingBasedAlgorithm ring = new RingBasedAlgorithm(4);
 		
-		RingBasedAlgorithm ring = new RingBasedAlgorithm(4);
+		PermissionBasedAlgorithm permission = new PermissionBasedAlgorithm(4);
 
 	}
 
--- src/main/PermissionBasedAlgorithm.java
+++ src/main/PermissionBasedAlgorithm.java
@@ -1,127 +1,128 @@
 package main;
 
 import java.util.LinkedList;
+import java.util.Queue;
+
+import main.RingBasedAlgorithm.MyNode;
 
 
 public class PermissionBasedAlgorithm {
-	//tamanho da fila
-	static int queueSize;    
+	
+	static boolean token;
+   
     //fila de itens
-    static LinkedList<MyNode> queue = new LinkedList<MyNode>();    
-    //fila de proximo
-    static int next[];
-    //contador de acessos a região crítica
-    static int criticalAreaCounter = 0;
+    static LinkedList<MyNode> nodes = new LinkedList<MyNode>();
+    
+    static Queue<MyNode> nodesWaitingAccess = new LinkedList<MyNode>();
     
     //construtor
     /**********************************************************/
     PermissionBasedAlgorithm(int size) { 
-    	
-    	this.queueSize = size;
-    	 next = new int[queueSize];
-    	
+    	token = true;
     	//põe os nodos na fila
-        for (int i = 0; i < queueSize; i++) {
+        for (int i = 0; i < size; i++) {
         	//cria o nodo
-            MyNode node = new MyNode(i, false);
+            MyNode node = new MyNode(i);
             //inicia o nodo
             node.start();
             //adiciona o nodo na fila
-            queue.add(node);
-            //define o ultimo elemento apontando pro primeiro
-            if(i == queueSize - 1) next[i] = 0;
-            else next[i] = i + 1;   
+            nodes.add(node);   
         }
-
-        
-        //inicia o token no primeiro nodo
-        MyNode first = queue.element();
-        first.receiveToken(true);
-     }  
+  	
+    }  
     
+    //requisição de acesso a região critica
+    static void requestAccessCritialArea(MyNode n) {
+    	//concede o acesso se a fila estiver vazia
+    	if(nodesWaitingAccess.isEmpty() && token) {
+    		token = false;
+    		n.conceedAccessCritialArea(token);
+    	} else {
+    		//adiciona o nodo a fila caso não esteja com o token
+    		nodesWaitingAccess.add(n);
+    	}
+    }
+    
+    //devolução do token pelo processo
+    /**********************************************************/
+    static void backToken(boolean t) {
+    	//recebe o token
+    	token = t;
+    	//verifica se tem nodos na fila
+    	if(!nodesWaitingAccess.isEmpty()) {
+    		///pega o proximo nodo da fila
+    		MyNode next = nodesWaitingAccess.poll();
+    		//concede o token para este nodo
+    		next.conceedAccessCritialArea(token);
+    	}
+    }
     
     //representação da região crítica
     /**********************************************************/
-    static void criticalArea(MyNode n) throws Exception {
-    	if(n.hasToken) {
-    		//criticalAreaCounter++;
-    		//System.out.println("=====");
-    		//System.out.println("Acessos simultâneos a área crítica: " + criticalAreaCounter);
-    		//System.out.println("=====");
-    		Thread.sleep(5000);
-    		//criticalAreaCounter--;
-    	} else {
-    		System.err.println("Erro de Acesso");
-    		throw new Exception("Acesso indevido a região crítica");
-    	}
-    	
+    static void criticalArea() throws Exception {
+    	Thread.sleep(5000);
     }
+    
+    
 
     
     /**
      *     Processo que vai na fila
      *
      */
+    
     /**********************************************************/
     static class MyNode extends Thread {
 
-        boolean hasToken;
-
+    	boolean token;
         int index;
+        boolean isAccessRequested;
    
-
         //construtor
         /**********************************************************/
-        MyNode(int i, boolean t) {
-            this.hasToken = t;
+        MyNode(int i) {
             this.index = i;
+            this.token = false;
+            this.isAccessRequested = false;
             System.out.println("[Nodo " + this.index + "] iniciado");
         }
 
-
-        //recebe o token
+        //tentativa de acesso a região crítica
         /**********************************************************/
-        public void receiveToken (boolean t) {
-        	System.out.println("-------------------------------------------");
-        	System.out.println("[Nodo " + this.index + "] recebendo o token");
-            this.hasToken = t;
-            
-            //tenta accessar a região crítica
-            try {
-                accessCritialArea();
-            } catch (Exception e) {
-                System.err.println("[Nodo " + this.index + "]" + e.getMessage());
-                System.exit(0);
-            //libera o token 
-            } finally {
-                this.passToken();
-            }
-            
+        private void requestAccessCritialArea() throws Exception {
+        	this.isAccessRequested = true;
+        	System.out.println("[Nodo " + this.index + "] Requisitando acesso a RC");
+        	PermissionBasedAlgorithm.requestAccessCritialArea(this);
         }
-
-
-        //passa o token para o próximo nodo
+        
+        //permissão de acesso a região crítica
         /**********************************************************/
-        private void passToken () {
-        	System.out.println("[Nodo " + this.index + "] passando o token");
-            int nextPos = next[index];
-            MyNode next = queue.get(nextPos);
-            next.receiveToken(this.hasToken);
-            this.hasToken = false;
+        public void conceedAccessCritialArea(boolean token) {
+        	this.token = token; 
+        	System.out.println("[Nodo " + this.index + "] Acesso concedido a RC");
+        	//acessa a RC
+        	try {
+        		PermissionBasedAlgorithm.criticalArea();
+        	} catch (Exception e) {
+        		System.err.println("[Nodo " + this.index + "] Erro na execução");
+        	}
+    		//devolve o token
+    		System.out.println("[Nodo " + this.index + "] devolvendo token");
+    		PermissionBasedAlgorithm.backToken(this.token);
         }
-  
-
-        //acesso a região crítica
-        /**********************************************************/
-        private void accessCritialArea() throws Exception {
-        	System.out.println("[Nodo " + this.index + "] Acessando região crítica");
-        	criticalArea(this);
-        }
-
         
         //inicia o processo
         /**********************************************************/
         public void run() {
+        	try {
+        		this.sleep(1000);
+        		
+        		//requisita o acesso a RC caso ainda nao tenha requisitado
+        		if(!isAccessRequested) this.requestAccessCritialArea();
+        		
+        	} catch (Exception e) {
+        		System.err.println( "[Nodo " + this.index + "]Erro na execução");
+        	}
         }
     }
 
